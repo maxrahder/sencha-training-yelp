aa0577fac88c8f862873a793981c6a0bc658aea6
Max Rahder <rahder@gmail.com>
2018-10-17 19:57:27 -0500

    Stub out UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
 import "./App.scss";
-import { Panel } from "@sencha/ext-react";
+import { Panel, Button, Img, Container, Toolbar } from "@sencha/ext-react";
+import { Component as ExtComponent } from "@sencha/ext-react";
+import logo from "../../public/images/YelpExtplorerLogo.png";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log(logo);
+  }
   render() {
     return (
-      <Panel
-        title="Main View"
-        padding={8}
-        html="<h1>This is the main view.</h1>"
-      />
+      <Container layout="hbox">
+        <Toolbar docked="top">
+          <Img docked="top" src={logo} height={47} />
+        </Toolbar>
+        <Panel title="Tab panel goes here" flex={1} border={true} />
+        <Panel title="Detail component goes here" width={130} border={true} />
+      </Container>
     );
   }
 }
-
 export default App;
